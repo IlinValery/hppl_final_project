152ad2e1530edfd1874a2b596144c6585d08f530
Elvira-Zainulina <zainulina.et@gmail.com>
2020-12-08 21:44:18 +0500

    Update utils.py

--- scripts/mpi4py_realisation/utils.py
+++ scripts/mpi4py_realisation/utils.py
@@ -3,4 +3,4 @@ def getSeed(key):
     key = key.lower()
     for i in range(len(key) - 1):
         keySeed *= ord(key[i]) * (ord(key[i]) - ord(key[i + 1]))
-    return abs(keySeed) % (2**32-1)
+    return keySeed % (2**32-1)
