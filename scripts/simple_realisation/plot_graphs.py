a639b1357b8c6e249bab73da132ab1c5d137bd36
stepan <steplerchik@gmail.com>
2020-12-09 16:58:10 +0300

    #32: add compare all graphs for simple_realisation

--- scripts/simple_realisation/plot_graphs.py
+++ scripts/simple_realisation/plot_graphs.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    basic_encryptor_times = []
+    basic_decryptor_times = []
     advanced_encryptor_times = []
     advanced_decryptor_times = []
     text_sizes = []
@@ -39,19 +41,37 @@ if __name__ == '__main__':
             data = line.split(" - ")
             advanced_decryptor_times.append(float(data[1]))
 
-    sorted_data = sorted(zip(text_sizes, advanced_encryptor_times, advanced_decryptor_times), key=lambda x: x[0])
-    text_sizes, advanced_encryptor_times, advanced_decryptor_times = zip(*sorted_data)
-    text_sizes = list(text_sizes)
-    advanced_encryptor_times = list(advanced_encryptor_times)
-    advanced_decryptor_times = list(advanced_decryptor_times)
-    print(text_sizes)
+    with open(args.basic_decryptor_times, "r") as f:
+        i = -1
+        for line in f:
+            i += 1
+            if i in missed_lines:
+                continue
+            data = line.split(" - ")
+            basic_decryptor_times.append(float(data[1]))
+
+    with open(args.basic_encryptor_times, "r") as f:
+        i = -1
+        for line in f:
+            i += 1
+            if i in missed_lines:
+                continue
+            data = line.split(" - ")
+            basic_encryptor_times.append(float(data[1]))
+
+    sorted_data = sorted(zip(text_sizes, advanced_encryptor_times, advanced_decryptor_times, basic_encryptor_times,
+                             basic_decryptor_times), key=lambda x: x[0])
+    text_sizes, advanced_encryptor_times, advanced_decryptor_times, basic_encryptor_times, basic_decryptor_times = zip(
+        *sorted_data)
     fig, ax = plt.subplots()
     ax.plot(text_sizes, advanced_encryptor_times, label="Advanced encryptor")
     ax.plot(text_sizes, advanced_decryptor_times, label="Advanced decryptor")
+    ax.plot(text_sizes, basic_encryptor_times, label="Basic encryptor")
+    ax.plot(text_sizes, basic_decryptor_times, label="Basic decryptor")
     ax.legend()
     ax.set_xlabel("Text size, number of letters")
     ax.set_ylabel("Time, secs")
-    ax.set_title("Advanced crypting results")
+    ax.set_title("Comparison of advanced and basic processing")
     plt.grid(zorder=0)
     fig.tight_layout()
     plt.savefig("Productivity comparison.png", bbox_inches='tight')
--- scripts/simple_realisation/run_compare.sh
+++ scripts/simple_realisation/run_compare.sh
@@ -4,7 +4,12 @@ FILES=../../data/texts/*
 for text in $FILES
 do
     filename=$(basename -- $text)
-    echo $filename
     python advanced/encryptor.py -i ../../data/fancy_guy.png -o ../../data/outputs/out.bmp -k ../../data/key.txt -f ../../data/texts/$filename
     python advanced/decryptor.py -i ../../data/outputs/out.bmp -k ../../data/key.txt -f ../../data/outputs/out_text.txt
+
+    python basic/encryptor.py -i ../../data/fancy_guy.png -o ../../data/outputs/out.bmp -f ../../data/texts/$filename
+    python basic/decryptor.py -ir ../../data/fancy_guy.png -ic ../../data/outputs/out.bmp -f ../../data/outputs/out_text.txt
+
 done
+
+python plot_graphs.py
